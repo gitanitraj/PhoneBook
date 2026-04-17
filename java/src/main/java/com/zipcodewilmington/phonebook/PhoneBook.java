3c7eef1f95f542f950e70efe0b2b982f1f549612
gitanitraj <anitra.com@gmail.com>
2026-04-17 15:38:37 -0400

    Added code provided in class

--- java/src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ java/src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,9 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
 import java.util.ArrayList;
-//import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by leon on 1/23/18.
@@ -14,41 +12,92 @@ import java.util.Map;
 public class PhoneBook {
 
     private final Map<String, List<String>> phonebook;
+    private final Map<String, String> reverse_phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
+        this.reverse_phonebook = new TreeMap<>();
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new TreeMap<>();
+        this.reverse_phonebook = new TreeMap<>();
     }
 
+    private void addRevNumber(String number, String name) {
+        this.reverse_phonebook.put(number, name);
+    }
     public void add(String name, String phoneNumber) {
+        if (this.hasEntry(name)) {
+            List<String> l = this.phonebook.get(name);
+            l.add(phoneNumber);
+            addRevNumber(phoneNumber, name);
+        } else {
+            List<String> l = new ArrayList<>();
+            l.add(phoneNumber);
+            addRevNumber(phoneNumber, name);
+            this.phonebook.put(name, l);
+        }
+    }
+    public List<String> lookup(String name) {
+        List<String> nums = this.phonebook.get(name);
+        return nums;
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        if (this.hasEntry(name)) {
+            List<String> l = this.phonebook.get(name);
+            loopThruAndAdd(l, name, phoneNumbers);
+        } else {
+            List<String> l = new ArrayList<>();
+            loopThruAndAdd(l, name, phoneNumbers);
+            this.phonebook.put(name, l);
+        }
+
+    }
+
+    private void loopThruAndAdd(List<String> foo, String name, String... strings) {
+        for (String s : strings) {
+            foo.add(s);
+            addRevNumber(s, name);
+        }
     }
 
     public void remove(String name) {
+        this.phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return this.phonebook.containsKey(name);
     }
 
-    public List<String> lookup(String name) {
-        return null;
-    }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String s = "not found.";
+
+        // loop thru the phonebook,
+        // get each name,
+        // loop thru the list of numbers
+        // if the number is found
+        // return the name
+        
+        return s;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        return new ArrayList<String>();
     }
 
     public Map<String, List<String>> getMap() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        PhoneBook pb = new PhoneBook();
+        List<String> phlist = pb.getAllContactNames();
+        System.out.println("My Phone Book:");
+        for (String name : phlist) {
+            System.out.println(name);
+        }
     }
 }
